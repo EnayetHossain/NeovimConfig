385caa1d484665bb2be7afbebc42982bce725879
EnayetHossain <enayethossasin135@gmail.com>
2026-05-29 08:13:56 +0600

    fixed virtual environment issue

--- lua/custom/plugins/swenv.lua
+++ lua/custom/plugins/swenv.lua
@@ -1,29 +1,35 @@
 return {
-  "AckslD/swenv.nvim",
-  config = function()
-    local swenv = require("swenv")
+	"AckslD/swenv.nvim",
+	config = function()
+		local swenv = require("swenv")
 
-    swenv.setup({
-      venvs_path = vim.fn.getcwd(),
-      post_set_venv = function()
-        vim.cmd("LspRestart")
-      end,
-    })
+		swenv.setup({
+			venvs_path = vim.fn.getcwd(),
+			post_set_venv = function()
+				-- vim.cmd("LspRestart")
+				-- LspRestart was removed in Neovim 0.12; restart LSP clients manually
+				local clients = vim.lsp.get_clients({ bufnr = 0 })
+				for _, client in ipairs(clients) do
+					vim.lsp.stop_client(client.id)
+				end
+				vim.cmd("edit")
+			end,
+		})
 
-    -- 🔧 Register the :SwenvInfo command correctly
-    vim.api.nvim_create_user_command("SwenvInfo", function()
-      local api = require("swenv.api")
-      local venv = api.get_current_venv()
-      if venv then
-        vim.notify("Current virtualenv: " .. venv.name, vim.log.levels.INFO)
-      else
-        vim.notify("No virtual environment selected", vim.log.levels.WARN)
-      end
-    end, {})
-  end,
+		-- 🔧 Register the :SwenvInfo command correctly
+		vim.api.nvim_create_user_command("SwenvInfo", function()
+			local api = require("swenv.api")
+			local venv = api.get_current_venv()
+			if venv then
+				vim.notify("Current virtualenv: " .. venv.name, vim.log.levels.INFO)
+			else
+				vim.notify("No virtual environment selected", vim.log.levels.WARN)
+			end
+		end, {})
+	end,
 
-  keys = {
-    { "<leader>vs", "<cmd>lua require('swenv.api').pick_venv()<CR>", desc = "Select VirtualEnv" },
-    { "<leader>vi", "<cmd>SwenvInfo<CR>", desc = "Show Current VirtualEnv" },
-  },
+	keys = {
+		{ "<leader>vs", "<cmd>lua require('swenv.api').pick_venv()<CR>", desc = "Select VirtualEnv" },
+		{ "<leader>vi", "<cmd>SwenvInfo<CR>", desc = "Show Current VirtualEnv" },
+	},
 }
